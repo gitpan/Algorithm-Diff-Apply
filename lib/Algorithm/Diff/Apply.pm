4c5827e67c6c7949cadae6592390523f7a7b8bde
Andrew Chadwick <and-x-rewc@pi-y-ffle.org.z>
2004-11-03 12:28:55 +0000

    Import of ANDREWC/Algorithm-Diff-Apply-0.2.3 from CPAN.
    gitpan-cpan-distribution: Algorithm-Diff-Apply
    gitpan-cpan-version:      0.2.3
    gitpan-cpan-path:         ANDREWC/Algorithm-Diff-Apply-0.2.3.tar.gz
    gitpan-cpan-author:       ANDREWC
    gitpan-cpan-maturity:     released

--- lib/Algorithm/Diff/Apply.pm
+++ lib/Algorithm/Diff/Apply.pm
@@ -11,7 +11,10 @@ use vars qw{@EXPORT_OK $VERSION};
 	mark_conflicts
 	optimise_remove_duplicates
 };
-$VERSION = '0.2.1';
+$VERSION = '0.2.3';
+#           ^     incr. implies loss of backwards compatibility, no workaround
+#             ^   increment implies a new feature, or big under-bonnet changes
+#               ^ this gets incremented on bugfixes and minor
 
 
 # Apply a single diff sequence. Nice and simple, and doesn't require
@@ -48,7 +51,6 @@ sub apply_diff
 }
 
 
-
 # Apply one or more labelled diff sequences to a target array.
 # Somewhat more complex; needs prepasses and consideration of
 # conflicts.
@@ -62,13 +64,12 @@ sub apply_diffs
 	my %diffset;
 	while (my $tag = shift)
 	{
-		ref($tag) and die("Tagnames must be scalar");
+		ref($tag) and croak("Tagnames must be scalar");
 		my $diff = shift;
 		ref($diff) eq 'ARRAY'
 			or croak("Diff sequences must be references of "
 				 . "type \"ARRAY\"");
-		$diff = [__homogenise_diff(@$diff)];
-		$diffset{$tag} = $diff;
+		$diffset{$tag} = __homogenise_diff($diff, %opt);
 	}
 
 	# Trivial case
@@ -77,7 +78,61 @@ sub apply_diffs
 		return wantarray ? @ary : \@ary;
 	}
 
-	# First pass: detect conflicts and optimise them
+	my @alts = __optimise_conflicts(diffset => \%diffset,
+	                                opts => \%opt);
+	__apply_alternatives(target => \@ary,
+	                     alts => \@alts,
+			     opts => \%opt);
+	return wantarray ? @ary : \@ary;
+}
+
+
+# Converts all the hunks in an Algorithm::Diff-style diff to a
+# normalised form in which all hunks are a) still internally
+# contiguous, and b) have start indices which refer to items in the
+# original array, before any diffs are applied. Normally, hunks
+# consisting of only inserts don't meet criterion b).
+#
+# Allso attaches hash data if the hashing function is defined.
+
+sub __homogenise_diff
+{
+	my ($orig_diff, %opt) = @_;
+	my @hdiff = ();
+	my $delta = 0;   # difference between orig and resultant
+	foreach my $orig_hunk (@$orig_diff)
+	{
+		my ($first_op, $start) = @{$orig_hunk->[0]} [0, 1];
+		$start -= $delta  if $first_op eq '+';
+		my $hhunk = {
+			start => $start,
+			changes => [],
+		};
+		foreach my $change (@$orig_hunk)
+		{
+			my ($op, $data);
+			($op, undef, $data) = @$change;
+			$delta += (($op eq '+') ? 1 : -1);
+			my $hash = (exists($opt{key_generator})
+				    ? $opt{key_generator}->($data)
+				    : undef);
+			push @{$hhunk->{changes}}, [$op, $data, $hash];
+		}
+		push @hdiff, $hhunk;
+	}
+	return \@hdiff;
+}
+
+
+# Calls the specified optimisation callbacks, returning a list of discrete
+# alternative blocks in a format that __apply_alternatives() can handle.
+
+sub __optimise_conflicts
+{
+	my %args = @_;
+	my %diffset = %{$args{diffset} || confess "\"diffset\" not specified"};
+	my %opt = %{$args{opts} || confess "\"opts\" not specified"};
+
 	my @optim;
 	if ($opt{optimisers} or $opt{optimizers})
 	{
@@ -117,207 +172,8 @@ sub apply_diffs
 			push(@alts, [$o_min, $o_max, %o_alt]);
 		}
 	}
-	
-	# Second pass: optimise them, then apply hunks one by one to
-	# generate the new array.
-	my $resolver = $opt{resolver} || \&mark_conflicts;
-
-	my $delta = 0;
-	while (my $alt = shift @alts)
-	{
-		my ($min, $max, %alts) = @$alt;
-		my @orig = @ary[$min + $delta .. $max + $delta - 1];
-		my @replacement;
-
-		my %alt_txts;
-		foreach my $id (sort keys %alts)
-		{
-			my @tmp = @orig;
-			my $tmp_delta = -$min;
-			foreach my $hunk (@{ $alts{$id} })
-			{
-				__apply_hunk(\@tmp, \$tmp_delta, $hunk);
-			}
-			$alt_txts{$id} = \@tmp;
-		}
-		
-		if (scalar keys %alt_txts == 1)
-		{
-			my ($r) = values %alt_txts;
-			@replacement = @$r;
-		}
-		else
-		{
-			@replacement = $resolver->(src_range_end => $max,
-						   src_range_start => $min,
-						   src_range => \@orig,
-						   alt_txts => \%alt_txts,
-						   invoc_opts => \%opt);
-		}		
-		splice(@ary, $min + $delta, $#orig+1, @replacement);
-		$delta += ($#replacement - $#orig);
-	}
-
-	return wantarray ? @ary : \@ary;
+	return @alts;
 }
-
-
-# Discard empty lists from a diffset.
-
-sub __diffset_discard_empties
-{
-	my %dset = @_;
-	return map {
-		($#{$dset{$_}} < 0) ? () : ($_ => $dset{$_});
-	} keys %dset;
-}
-
-
-
-
-# The default conflict resolution subroutine. Returns all alternative
-# texts with conflict markers inserted around them.
-
-sub mark_conflicts
-{
-	my %opt = @_;
-	defined $opt{alt_txts} or confess("alt_txts not defined\n");
-	my %alt = %{$opt{alt_txts}};
-	my @ret;
-	foreach my $id (sort keys %alt)
-	{
-		push @ret, ">>>>>> $id\n";
-		push @ret, @{$alt{$id}};
-	}
-	push @ret, "<<<<<<\n";
-	return @ret;
-}
-
-
-sub optimise_remove_duplicates
-{
-	my %opt = @_;
-	my $block = $opt{conflict_block};
-	defined $block or confess("conflict_block not defined\n");
-	my @tags = reverse sort keys(%$block);
-	my %ret = map {$_ => []} @tags;
-    REFTAG:
-	while (my $tag = shift @tags)
-	{
-	    REFHUNK:
-		for my $hunk (@{$block->{$tag}})
-		{
-			for my $t (@tags)
-			{
-				for my $h (@{$block->{$t}})
-				{
-					__hunks_identical($hunk, $h)
-						and next REFHUNK;
-				}
-			}
-			push @{$ret{$tag}}, $hunk;
-		}
-	}
-	#$block = _semi_deep_copy($block);
-
-	return %ret;
-}
-
-
-sub __dump_diffset
-{
-	my %dset = @_;
-	print STDERR "-- begin diffset --\n";
-	for my $tag (sort keys %dset)
-	{
-		print STDERR "-- begin seq tag=\"$tag\" --\n";
-		my @diff = @{$dset{$tag}};
-		for my $diff (@diff)
-		{
-			print STDERR "\n\@".$diff->{start}."\n";
-			for my $e (@{$diff->{changes}})
-			{
-				my ($op, $data) = @$e;
-				$data = quotemeta($data);
-				$data =~ s{^(.{0,75})(.*)}{
-					$1 . ($2 eq "" ? "" : "...");
-				}se;
-				print STDERR "$op $data\n";
-			}
-		}
-		print STDERR "\n-- end seq tag=\"$tag\" --\n";
-	}	
-	print STDERR "-- end diffset --\n";
-}
-
-
-
-sub __hunks_identical
-{
-	my ($h1, $h2) = @_;
-	$h1->{start} == $h2->{start} or return 0;
-	$#{$h1->{changes}} == $#{$h2->{changes}} or return 0;
-	my $i;
-	for ($i=0; $i < $#{$h1->{changes}}; ++$i)
-	{
-		my ($op1, $data1) = @{ $h1->{changes}->[$i] };
-		my ($op2, $data2) = @{ $h2->{changes}->[$i] };
-		$op1 eq $op2 or return 0;
-		$data1 eq $data2 or return 0; # XXX FIXME XXX
-	}
-	return 1;
-}
-
-
-# Deep-copies a simple structure of simple Perl structures, copying
-# arrays, scalars, and hashes, and leaving filehandles, blessed refs
-# and anything else alone.
-
-sub _semi_deep_copy
-{
-	my $struct = shift;
-	my $type = ref($struct);
-	return $struct unless defined($type);
-	if ($type eq 'ARRAY')
-	{
-		my @ary;
-		foreach my $el (@$struct)
-		{
-			push(@ary, _semi_deep_copy($el));
-		}
-		return \@ary;
-	}
-	elsif ($type eq 'HASH')
-	{
-		my %hash;
-		while (my ($k, $v) = each %$struct)
-		{
-			$hash{$k} = _semi_deep_copy($v);
-		}
-		return \%hash;
-	}
-	elsif ($type eq 'SCALAR')
-	{
-		return _semi_deep_copy($$struct);
-	}
-	else
-	{
-		# leave it the hell alone
-		return $struct;
-	}
-}
-
-
-# *Terminology*
-#
-# A "diffset" is a hash of IDs whose values are arrays holding
-# sequences of diffs generated from different sources. There may be
-# conflicts within a diffset.
-# 
-# An "alternatives" diffset is a minimal diffset which contains no
-# more than one conflict. I can't think of a better name for it, as
-# there's a special case where it only consists of a single key
-# pointing at a single hunk.
 
 
 # Extracts the array ($min, $max, %alts) from %$diffset where $min and
@@ -366,30 +222,6 @@ sub __shift_next_alternatives
 }
 
 
-# Applies a hunk to an array, and calculates the lines lost or gained
-# by doing so.
-
-sub __apply_hunk
-{
-        my ($ary, $rdelta, $hunk) = @_;
-	my $pos = $hunk->{start} + $$rdelta;
-        foreach my $change (@{$hunk->{changes}})
-        {
-                if ($change->[0] eq '+')
-                {
-                        splice(@$ary, $pos, 0, $change->[1]);
-                        ++$$rdelta;
-                        ++$pos;
-                }
-                else
-                {
-                        splice(@$ary, $pos, 1);
-                        --$$rdelta;
-                }
-        }
-}
-
-
 # Returns the ID of the hunk in %$diffset whose ->{start} is lowest,
 # or undef. %ignore{SOMEID} can be set to a true value to cause a
 # given sequence to be skipped over.
@@ -415,35 +247,191 @@ sub __next_hunk_id
 }
 
 
-# Converts all the hunks in an Algorithm::Diff-style diff to a
-# normalised form in which all hunks are a) still internally
-# contiguous, and b) have start indices which refer to items in the
-# original array, before any diffs are applied. Normally, hunks
-# consisting of only inserts don't meet criterion b).
-
-sub __homogenise_diff
+sub __diffset_discard_empties
 {
-	my @hdiff = ();
-	my $delta = 0;   # difference between orig and resultant
-	foreach my $orig_hunk (@_)
-	{
-		my ($first_op, $start) = @{$orig_hunk->[0]} [0, 1];
-		$start -= $delta  if $first_op eq '+';
-		my $hhunk = {
-			start => $start,
-			changes => [],
-		};
-		foreach my $change (@$orig_hunk)
-		{
-			my ($op, $data);
-			($op, undef, $data) = @$change;
-			$delta += (($op eq '+') ? 1 : -1);
-			push @{$hhunk->{changes}}, [$op, $data];
-		}
-		push @hdiff, $hhunk;
-	}
-	return @hdiff;
+	my %dset = @_;
+	return map {
+		($#{$dset{$_}} < 0) ? () : ($_ => $dset{$_});
+	} keys %dset;
 }
 
+
+sub __apply_alternatives
+{
+	my %args = @_;
+	my %opt = %{$args{opts} || confess "\"opts\" not specified"};
+	my $ary = $args{target} || confess "\"target\" not specified";
+	my @alts = @{$args{alts} || confess "\"alts\" not specified"};
+	my $resolver = $opt{resolver} || \&mark_conflicts;
+
+	my $delta = 0;
+	while (my $alt = shift @alts)
+	{
+		my ($min, $max, %alts) = @$alt;
+		my @orig = @{$ary}[$min + $delta .. $max + $delta - 1];
+		my @replacement;
+
+		my %alt_txts;
+		foreach my $id (sort keys %alts)
+		{
+			my @tmp = @orig;
+			my $tmp_delta = -$min;
+			foreach my $hunk (@{ $alts{$id} })
+			{
+				__apply_hunk(\@tmp, \$tmp_delta, $hunk);
+			}
+			$alt_txts{$id} = \@tmp;
+		}
+		
+		if (scalar keys %alt_txts == 1)
+		{
+			my ($r) = values %alt_txts;
+			@replacement = @$r;
+		}
+		else
+		{
+			@replacement = $resolver->(src_range_end => $max,
+						   src_range_start => $min,
+						   src_range => \@orig,
+						   alt_txts => \%alt_txts,
+						   invoc_opts => \%opt);
+		}		
+		splice(@$ary, $min + $delta, $#orig+1, @replacement);
+		$delta += ($#replacement - $#orig);
+	}
+}
+
+
+# Applies a hunk to an array, and calculates the lines lost or gained
+# by doing so.
+
+sub __apply_hunk
+{
+        my ($ary, $rdelta, $hunk) = @_;
+	my $pos = $hunk->{start} + $$rdelta;
+        foreach my $change (@{$hunk->{changes}})
+        {
+                if ($change->[0] eq '+')
+                {
+                        splice(@$ary, $pos, 0, $change->[1]);
+                        ++$$rdelta;
+                        ++$pos;
+                }
+                else
+                {
+                        splice(@$ary, $pos, 1);
+                        --$$rdelta;
+                }
+        }
+}
+
+
+# The default conflict resolution subroutine. Returns all alternative
+# texts with conflict markers inserted around them.
+
+sub mark_conflicts (%)
+{
+	my %opt = @_;
+	defined $opt{alt_txts} or confess("alt_txts not defined\n");
+	my %alt = %{$opt{alt_txts}};
+	my @ret;
+	foreach my $id (sort keys %alt)
+	{
+		push @ret, ">>>>>> $id\n";
+		push @ret, @{$alt{$id}};
+	}
+	push @ret, "<<<<<<\n";
+	return @ret;
+}
+
+
+sub optimise_remove_duplicates (%)
+{
+	my %opt = @_;
+	my $block = $opt{conflict_block};
+	defined $block or confess("conflict_block not defined\n");
+	my @tags = reverse sort keys(%$block);
+	my %ret = map {$_ => []} @tags;
+    REFTAG:
+	while (my $tag = shift @tags)
+	{
+	    REFHUNK:
+		for my $hunk (@{$block->{$tag}})
+		{
+			for my $t (@tags)
+			{
+				for my $h (@{$block->{$t}})
+				{
+					__hunks_identical($hunk, $h)
+						and next REFHUNK;
+				}
+			}
+			push @{$ret{$tag}}, $hunk;
+		}
+	}
+	return %ret;
+}
+
+
+sub __hunks_identical
+{
+	my ($h1, $h2) = @_;
+	$h1->{start} == $h2->{start} or return 0;
+	$#{$h1->{changes}} == $#{$h2->{changes}} or return 0;
+	foreach my $i (0 .. $#{$h1->{changes}})
+	{
+		my ($op1, $data1, $hash1) = @{ $h1->{changes}->[$i] };
+		my ($op2, $data2, $hash2) = @{ $h2->{changes}->[$i] };
+		$op1 eq $op2 or return 0;
+		if (defined($hash1) && defined($hash2))
+		{
+			$hash1 eq $hash2 or return 0;
+		}
+		else
+		{
+			$data1 eq $data2 or return 0;
+		}
+	}
+	return 1;
+}
+
+
+sub __dump_diffset
+{
+	my %dset = @_;
+	print STDERR "-- begin diffset --\n";
+	for my $tag (sort keys %dset)
+	{
+		print STDERR "-- begin seq tag=\"$tag\" --\n";
+		my @diff = @{$dset{$tag}};
+		for my $diff (@diff)
+		{
+			print STDERR "\n\@".$diff->{start}."\n";
+			for my $e (@{$diff->{changes}})
+			{
+				my ($op, $data) = @$e;
+				$data = quotemeta($data);
+				$data =~ s{^(.{0,75})(.*)}{
+					$1 . ($2 eq "" ? "" : "...");
+				}se;
+				print STDERR "$op $data\n";
+			}
+		}
+		print STDERR "\n-- end seq tag=\"$tag\" --\n";
+	}	
+	print STDERR "-- end diffset --\n";
+}
+
+
+# *Terminology*
+#
+# A "diffset" is a hash of IDs whose values are arrays holding
+# sequences of diffs generated from different sources. There may be
+# conflicts within a diffset.
+# 
+# An "alternatives" diffset is a minimal diffset which contains no
+# more than one conflict. I can't think of a better name for it, as
+# there's a special case where it only consists of a single key
+# pointing at a single hunk.
 
 1;
